9e26a70ee13d16ebfea2133bd638cb637d63f58c
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-06-27 11:16:04 +0300

    Add files via upload

--- MOD-5_module5hard.py
+++ MOD-5_module5hard.py
@@ -86,22 +86,24 @@ class UrTube:
             print(f"{second + 1}: воспроизведение видео...")
         print("Конец видео")
 
-ur = UrTube()
 
-v1 = Video('Лучший язык программирования 2024 года', 200)
-v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
+if __name__ == '__main__':
+   ur = UrTube()
 
-ur.add(v1, v2)
+   v1 = Video('Лучший язык программирования 2024 года', 200)
+   v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
 
-print(ur.get_videos('лучший'))
-print(ur.get_videos('ПРОГ'))
+   ur.add(v1, v2)
 
-ur.watch_video('Для чего девушкам парень программист?')
-ur.register('vasya_pupkin', 'lolkekcheburek', 13)
-ur.watch_video('Для чего девушкам парень программист?')
-ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
-ur.watch_video('Для чего девушкам парень программист?')
-ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
-print(ur.current_user)
+   print(ur.get_videos('лучший'))
+   print(ur.get_videos('ПРОГ'))
 
-ur.watch_video('Лучший язык программирования 2024 года!')
+   ur.watch_video('Для чего девушкам парень программист?')
+   ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+   ur.watch_video('Для чего девушкам парень программист?')
+   ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+   ur.watch_video('Для чего девушкам парень программист?')
+   ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+   print(ur.current_user)
+
+   ur.watch_video('Лучший язык программирования 2024 года!')
